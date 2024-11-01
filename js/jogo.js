a7381f969d8fa14555879ca84abfc8cbd5e0a29a
Anna Gabriela <agmsouza23@gmail.com>
2024-11-01 15:22:06 -0300

    Criação da navbar e ajuste no teclado

--- js/jogo.js
+++ js/jogo.js
@@ -22,49 +22,49 @@ let contadorDeErros = 0;
 
 const contextoSelecionado = localStorage.getItem('contextoSelecionado');
 
-if(!contextoSelecionado){
+if (!contextoSelecionado) {
     alert("Nenhum contexto foi selecionado");
     window.location.href = 'contextos.html';
-} else{
+} else {
     iniciarDesafio();
 }
 
-function iniciarDesafio(){
+function iniciarDesafio() {
     fetch('repositorio-palavras/palavras.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erro HTTP: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const contextos = data.contextos;
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const contextos = data.contextos;
+            const contextoAtual = contextos.find(contexto => contexto.nome.toLowerCase() === contextoSelecionado.toLowerCase());
+            let palavraSorteada;
 
-        const contextoAtual = contextos.find(contexto => contexto.nome.toLowerCase() === contextoSelecionado.toLowerCase());
+            document.getElementById('tema').innerHTML = contextoAtual.nome.toUpperCase();
 
-        let palavraSorteada;
+            do {
+                palavraSorteada = contextoAtual.palavras[Math.floor(Math.random() * contextoAtual.palavras.length)];
+                palavraSecreta = palavraSorteada.nome;
+            } while (palavrasSorteadas.includes(palavraSecreta));
 
-        do{
-            palavraSorteada = contextoAtual.palavras[Math.floor(Math.random() * contextoAtual.palavras.length)];
-            palavraSecreta = palavraSorteada.nome;
-        } while(palavrasSorteadas.includes(palavraSecreta));
+            if (!palavrasSorteadas.includes(palavraSecreta)) {
+                palavrasSorteadas.push(palavraSecreta);
+            }
 
-        if (!palavrasSorteadas.includes(palavraSecreta)){
-            palavrasSorteadas.push(palavraSecreta);
-        }
+            palavraAtual = Array(palavraSecreta.length).fill("_"); 
 
-        palavraAtual = Array(palavraSecreta.length).fill("_"); 
+            console.log("Palavra secreta sorteada:", palavraSecreta);
+            document.getElementById('imagem-jogo').src = palavraSorteada.imagem;
+            document.getElementById('imagem-jogo').onerror = function () {
+                this.src = "/img/error.png";
+            }
+            
+            exibirPalavra(); 
 
-        console.log("Palavra secreta sorteada:", palavraSecreta);
-        document.getElementById('imagem-jogo').src = palavraSorteada.imagem;
-        document.getElementById('imagem-jogo').onerror = function(){
-            this.src = "/img/error.png";
-        }
-        
-        exibirPalavra(); 
-
-     })
-    .catch(error => console.error('Erro ao carregar o JSON:', error));
+        })
+        .catch(error => console.error('Erro ao carregar o JSON:', error));
 }
 
 function exibirPalavra() {
@@ -96,12 +96,6 @@ function isConsoante(letra) {
     return !isVogal(letra);
 }
 
-function desabilitarBotao(botao) {
-    botao.disabled = true;
-    botao.style.backgroundColor = 'grey';
-    botao.style.color = 'white';
-}
-
 function letraClicada(letra) {
     let acertou = false;
     
@@ -113,29 +107,39 @@ function letraClicada(letra) {
             acertou = true;
         }
     } 
-    if(!acertou){
+    
+    if (!acertou) {
         contadorDeErros += 1;
     }
     
     exibirPalavra();
     
-    const botoes = document.querySelectorAll(`button[onclick="letraClicada('${letra}')"]`);;
-    botoes.forEach(desabilitarBotao);
+    const botoes = document.querySelectorAll(`button[onclick="letraClicada('${letra}')"]`);
+    botoes.forEach(botao => {
+        if (acertou) {
+            botao.style.backgroundColor = 'green';  // Define a cor verde para acertos
+            botao.style.color = 'white';
+        } else {
+            botao.style.backgroundColor = 'red';    // Define a cor vermelha para erros
+            botao.style.color = 'white';
+        }
+        botao.disabled = true;  // Desabilita o botão
+    });
 
-    if(!palavraAtual.includes("_")){
+    if (!palavraAtual.includes("_")) {
         proximaRodada();
     }
 }
 
-function proximaRodada(){
-    quantidadeDesafiosJogados ++;
+function proximaRodada() {
+    quantidadeDesafiosJogados++;
 
-    if (quantidadeDesafiosJogados < quantidadeDesafios){
+    if (quantidadeDesafiosJogados < quantidadeDesafios) {
         setTimeout(() => {
             resetarBotoes();
             iniciarDesafio();
         }, 1000);
-    } else{
+    } else {
         setTimeout(() => {
             palavrasSorteadas = [];
             exibirPontuacao();
@@ -143,22 +147,21 @@ function proximaRodada(){
     }
 }
 
-function resetarBotoes(){
+function resetarBotoes() {
     const botoes = document.querySelectorAll('button[onclick^="letraClicada"]');
-
-    botoes.forEach(botao =>{
+    botoes.forEach(botao => {
         botao.disabled = false;
         botao.style.color = '';
         botao.style.backgroundColor = '';
-    }
-    );
+    });
 }
 
-function exibirPontuacao(){
+function exibirPontuacao() {
     // Salva o número de erros no localStorage
     localStorage.setItem('contadorDeErros', contadorDeErros);
         
     // Redireciona para a página de pontuação
     window.location.href = 'pontuacao.html';
-
 }
+
+
